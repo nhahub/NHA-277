56ab800ab4d6df600682e234989d0ac4813fad07
Mohamed Waleed <mohamedwalidy762@gmail.com>
2025-10-09 18:54:28 +0300

    Initial commit - setup HomeFragment and ViewModel

--- app/src/main/java/com/mustafa/myapplication/Search/SearchNetwork/SearchRepo.kt
+++
@@ -1,7 +0,0 @@
-package com.mustafa.myapplication.Search.SearchNetwork
-
-import com.mustafa.myapplication.model.Movie
-
-interface SearchRepo {
-    suspend fun searchMovies(query : String) : List<Movie>
-}
