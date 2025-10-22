e61c2d8dc79951fa37f9efd844aefcedd6298511
Mohamed Waleed <mohamedwalidy762@gmail.com>
2025-10-22 18:39:18 +0300

    Initial commit - setup HomeFragment and ViewModel

--- app/src/main/java/com/mustafa/myapplication/Search/UiSearchState.kt
+++ app/src/main/java/com/mustafa/myapplication/Search/UiSearchState.kt
@@ -9,7 +9,7 @@ sealed class UiSearchState<out T>{
     object Empty : UiSearchState<Nothing>() // no Movie list found
 
     object Loading : UiSearchState<Nothing>() // While getting the list from the network
-    data class Success<T>(val movies : List<T>) : UiSearchState<Nothing>() // successfully get the list
+    data class Success<T>(val movies : List<T>) : UiSearchState<List<T>>() // successfully get the list
     data class Error(val message : String) : UiSearchState<Nothing>() // error happened
 }
 
